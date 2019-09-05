20e93ee2f5c97c6878204c0b648ced08f2ee5ed4
Paul Pena <Paul_Pena@claro.com.do>
2019-09-05 14:37:43 -0400

    Add Customer in app

--- ClientApp/boot.ts
+++ ClientApp/boot.ts
@@ -9,7 +9,9 @@ Vue.use(BootstrapVue);
 const routes = [
     { path: '/', component: require('./components/home/home.vue.html') },
     { path: '/counter', component: require('./components/counter/counter.vue.html') },
-    { path: '/fetchdata', component: require('./components/fetchdata/fetchdata.vue.html') }
+    { path: '/fetchdata', component: require('./components/fetchdata/fetchdata.vue.html') },
+    { path: '/customer', component: require('./components/customer/customer.vue.html') }
+
 ];
 
 new Vue({
